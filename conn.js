a4e9a21d04d213a91569257dbe10b8455b513d0c
Mehul-k123 <136571175+Mehul-k123@users.noreply.github.com>
2023-06-14 21:01:09 +0530

    Add files via upload

---
+++ conn.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+// import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/ChatbotDb',{useNewUrlParser: true}
+).then(()=>{
+    console.log("Connected Sucessfully");
+}).catch(()=>{
+    console.log("No connection");
+})
